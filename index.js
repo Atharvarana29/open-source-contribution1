2884fc708fd71b2e685e05768082b3ea750dec3d
Atharva Rana <your-email@example.com>
2025-04-12 14:56:26 +0530

    added the procedure that how to fork repo , do changes and commit and push in github

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("LMAO")
+// this is what i have learnt
+// first you have to do fork in the repo (of someone's that in which 
+// you want to contribute ) . then choose the name of the repo  that you want to keep
+// this will be  the name of your repository in which you will be doing the changes ,
+// because you can't directly import the repo and start doing the changes
+// the click on create a repo ,  now a repo of your choice name has got created and you can do changes in it
+// we do not do the changes directly in git hub as it just  offers code base and repo managment like featuers
+// so we import that repo which we have forked and renamed , to vs code folder throug git
+// we use command git clone <forked repo url> and then the vs code automatically clone of the files and folders 
+// of the repo with the custom name given by us to the repo in the choosen folder .
+// now here we can do what ever changes we want to do in different files and folders 
+// for eg:- in the html file , react file and all...
+// the with all the changes done (here perfer to do one specific change at a time , so  that even if it does 
+// not work or has some issues in the future then also then come to this specific commit id and correct it )
+// and now as usual we have written the code so now we have to add it and then commit it and then push it .
+// to github. 
+// 
+
+
+console.log("this is the first fork , that i am doing . have written all the things above")
